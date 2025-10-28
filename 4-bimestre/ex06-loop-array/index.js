203ccf9bfc325b24257b03e8c4820ad7b770fffa
Larissa Ramos <maria.sousa6250@aluno.ce.gov.br>
2025-10-28 14:51:23 +0000

    update

--- 4-bimestre/ex06-loop-array/index.js
+++ 4-bimestre/ex06-loop-array/index.js
@@ -1,4 +1,18 @@
 // CRIE UMA LÓGICA AQUI
 
-// === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
-module.exports = { filmes }
+const prompt = require('prompt-sync')();
+
+console.log("=== Sistema de Controle de Produção ===");
+
+// --- Lê a quantidade de dias ---
+const qtdDias = Number(prompt("Quantos dias deseja registrar? "));
+
+// --- Lê as produções diárias ---
+let producoes = [];
+
+for (let i = 1; i <= qtdDias; i++) {
+  let producaoDia = Number(prompt(`Digite a quantidade produzida no dia ${i}: `));
+
+
+// === NÃO MODIFIQUE NADA ABAIXO ============
+module.exports = resultado
--- 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
+++ 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
@@ -1,1 +1,14 @@
 // CRIE UMA LÓGICA ABAIXO
+
+let prompt = require("prompt-sync")()
+const producaoDia = []
+
+let pergunta = parseInt(prompt("quantos dias deseja registrar?"))
+for (let i = 0; 1 < pergunta; 1++){
+    pergunta.push(parseInt(prompt("Digite a quantidade produzida no dia 1:")))   
+}
+
+let somaArry = 0 
+for( i = 0 ; < pergunta; i++){
+    pergunta.push(parseInt(prompt("Digite a ")))
+}   
